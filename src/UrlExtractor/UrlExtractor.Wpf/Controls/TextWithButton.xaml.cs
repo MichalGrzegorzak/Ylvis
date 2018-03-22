85ea5b5c2e46c6def65231af5977fef8f3a46dea
mgrzegorzak <mgrzegorzak@gmail.com>
2018-03-22 08:02:40 +0000

    TextWithButton

---
+++ src/UrlExtractor/UrlExtractor.Wpf/Controls/TextWithButton.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using UrlExtractor.Wpf.ViewModel;
+
+namespace UrlExtractor.Wpf.Controls
+{
+    /// <summary>
+    /// Interaction logic for ItemDisplay.xaml
+    /// </summary>
+    public partial class TextWithButton : UserControl
+    {
+        public TextWithButton()
+        {
+            InitializeComponent();
+        }
+
+
+        private void SearchBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hi");
+        }
+    }
+}
