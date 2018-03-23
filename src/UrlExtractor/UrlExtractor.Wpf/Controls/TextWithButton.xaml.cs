1e706e3f2f05317f851212b99dcb2d8039511776
mgrzegorzak <mgrzegorzak@gmail.com>
2018-03-23 21:27:27 +0000

    textbox design

--- src/UrlExtractor/UrlExtractor.Wpf/Controls/TextWithButton.xaml.cs
+++ src/UrlExtractor/UrlExtractor.Wpf/Controls/TextWithButton.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using UrlExtractor.Wpf.ViewModel;
@@ -13,20 +14,33 @@ namespace UrlExtractor.Wpf.Controls
         public TextWithButton()
         {
             InitializeComponent();
-            SearchText  = "skopiuj mnie";
+            Text = "skopiuj mnie";
         }
 
-        public string SearchText { get; set; }
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(TextWithButton), new PropertyMetadata(string.Empty));
 
-
-        private void SearchBoxKeyDown(object sender, KeyEventArgs e)
+        static TextWithButton()
         {
-            //throw new System.NotImplementedException();
+            HeightProperty.OverrideMetadata(typeof(TextWithButton), new PropertyMetadata((double)22));
         }
+
+        //public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(string), typeof(TextWithButton), new FrameworkPropertyMetadata(String.Empty));
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Clipboard.SetText(txb.Text);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var expression = (sender as TextBox).GetBindingExpression(TextBox.TextProperty);
+            expression.UpdateSource();
         }
     }
 }
